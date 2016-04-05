19ca72abc6cd9690b98324820c50e571abddd72a
arwk1 <arturwlodarek@o2.pl>
2016-04-05 15:50:17 +0200

    v1
    uruchomienie skanowania sieci

---
+++ app/src/main/java/com/example/arwk/wifiscanner/Constants.java
@@ -0,0 +1,11 @@
+package com.example.arwk.wifiscanner;
+
+/**
+ * Created by arwk on 2016-04-05.
+ */
+
+public class Constants {
+    public static final String APP_NAME = "ANDROID_WIFI_SCANNER";
+    public static final String INTENT_FILTER = "ANDROID_WIFI_SCANNER";
+    public static final String WIFI_DATA = "WIFI_DATA";
+}
